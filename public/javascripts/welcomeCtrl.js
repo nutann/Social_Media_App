e6b111f9fd2ade588a44a6e35b87636af1db4dfb
nutann <nutnaik@gmail.com>
2017-04-24 11:21:54 -0700

    basic chat implementation

--- public/javascripts/welcomeCtrl.js
+++ public/javascripts/welcomeCtrl.js
@@ -8,8 +8,9 @@
 
     app.controller('welcomeCtrl', function($state, $scope,socket, $stateParams, $sce) {
         $scope.users = [];
-        $scope.chattingwith;
+        $scope.chattingwith = {};
         $scope.messages = [];
+        $scope.message = "";
         socket.on('loggedin-users',function(data){
         	console.log("logged in users " +JSON.stringify(data));
         	$scope.users = data.filter(function(item){
@@ -20,16 +21,19 @@
         $scope.sendMessage = function(event,message) {
         	event.preventDefault();
         	console.log("message to be sent is " +message +"user is " +JSON.stringify($scope.chattingwith));
-        	
-        	socket.emit("message-sent",{
+        	var data = {
         		message : message,
         		user : $scope.chattingwith
-        	})
+        	}
+
+        	$scope.messages.push(data);
+        	 $scope.message = "";
+        	socket.emit("message-sent",data)
         }
 
         
         socket.on('message-received', function(data){
-        	console.log("message-received"+ JSON.stringify(data.message));
+        	console.log("message-received"+ JSON.stringify(data));
         	$scope.messages.push(data);
         });
         
@@ -38,23 +42,7 @@
         	 $scope.chattingwith = user;
 
 
-        	       var element = '<div class="popup-box" id="'+ user.email +'">';
-                element = element + '<div class="popup-head">';
-                element = element + '<div class="popup-head-left">'+ user.firstName +'</div>';
-                element = element + '<div class="popup-head-right"><a href="javascript:close_popup(\''+ user.email +'\');">&#10005;</a></div>';
-                element = element + '<div style="clear: both"></div></div><div class="popup-messages"></div>';
-               // element = element + '<div style="clear: both"></div></div><div class="popup-messages"></div>';
-                    
-      			element = element + ' <form ng-submit="sendMessage($event,message)"> <input class="input-sm form-control" ng-model="message" type="text" placeholder="Type a message...">'
-                   + '<input class="btn btn-default"  type="submit" value="Send"> </form>  </div>'
-          		
-          
-       
-      
-    
-
-                console.log("element is " +element)
-                 $scope.chathtml = $sce.trustAsHtml(element);
+        	      
      
                 //document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element;  
 
@@ -62,18 +50,6 @@
 
     });
 
-    app.directive('compileTemplate', function($compile, $parse){
-    return {
-        link: function(scope, element, attr){
-            var parsed = $parse(attr.ngBindHtml);
-            function getStringValue() { return (parsed(scope) || '').toString(); }
-
-            //Recompile if the template changes
-            scope.$watch(getStringValue, function() {
-                $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
-            });
-        }         
-    }
-});
+    
 
 })();
