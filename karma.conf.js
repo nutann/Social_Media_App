4e291265c7acfee7f81fce11c611767750e7c253
Sheetal <sheetalkrishna43@gmail.com>
2017-05-15 20:29:48 +0200

    Karma & Jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        './node_modules/angular/angular.js',
+        './node_modules/angular-route/angular-route.js',
+        './node_modules/angular-mocks/angular-mocks.js',
+
+        './views/app.js',
+        './public/javascripts/loginCtrl.spec.js'
+        
     ],
 
 
